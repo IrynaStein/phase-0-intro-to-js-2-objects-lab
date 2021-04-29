677c6fdb0c57ae4918ce3126227dcb1a8475c168
Iryna Stein <isophiestein@gmail.com>
2021-04-29 13:39:32 -0600

    Re-wrote old Lab

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-// Write your solution in this file!
+
+// To start, define a `employee` variable and assign it to an `Object` containing
+// `name` and `streetAddress` keys; you can use whatever values you like. Use
+// literal syntax to create your `Object`. Various updates will be applied to this
+// variable (destructively and non-destructively) in this lab.
 
 const employee = {
-    name: "Sam",
-    streetAddress: "11 Broadway"
+    name: "Bob",
+    address: "Los Angeles, CA"
+
+};
+
+// - `updateEmployeeWithKeyAndValue()`: this function should take in three
+//   arguments: a `employee` `Object`, a `key` and a `value`. This function should
+//   not mutate the `employee`; it should return a _new_ `Object` that has an
+//   updated `value` for the `key` passed in.
+
+// function updateEmployeeWithKeyAndValue(obj, key, value){
+//     let newObj = {...obj};
+//     newObj [key]= value;
+//     return newObj;
+// }
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return Object.assign ({}, obj, {[key]:value})
 }
-function updateEmployeeWithKeyAndValue(obj, key, value) {
-    return Object.assign ({}, employee, {[key]:value}) 
+// - `destructivelyUpdateEmployeeWithKeyAndValue()`: this function should work the
+//   same as `updateEmployeeWithKeyAndValue()` but it _should_ mutate the
+//   `employee` `Object` passed in.
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value
+    return obj;
+}
+// - `deleteFromEmployeeByKey()`: this function should take in a `employee`
+//   `Object` and a `key`. It should delete the property with that `key` from the
+//   `employee` `Object`. This should not mutate the original `employee` `Object`;
+//   it should return a _new_ `Object` that doesn't include the identified
+//   key-value pair.
+function deleteFromEmployeeByKey(obj, key){
+    let newObj = {...obj};
+    delete newObj[key];
+    return newObj;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key]=value
-    return employee;
+// - `destructivelyDeleteFromEmployeeByKey()`: this function should work the same
+//   as `deleteFromEmployeeByKey()` but it _should_ mutate the `employee` `Object`.
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
 }
-function deleteFromEmployeeByKey(employee, key){
-    let newEmployee = {...employee};
-    delete newEmployee[key];
-    return newEmployee;
-}
-
-
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee [key]
-    return employee;
-}
-
-
-
-
